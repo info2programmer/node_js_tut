cd998e4ce3785a79ef19fa6d53d5aefd222ab40b
Saikat Bhadury <info2programmer@gmail.com>
2024-12-05 08:51:45 +0530

    module exports

---
+++ math.js
@@ -0,0 +1,12 @@
+/**
+ * Adds two numbers together.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+const add = (a, b) => {
+  return a + b;
+};
+
+module.exports = add;
