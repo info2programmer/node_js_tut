aa33629b486f582705573119bed26b56fcecd61a
Saikat Bhadury <info2programmer@gmail.com>
2024-12-21 12:32:14 +0530

    Add documentation for async directory reading function in FS module

--- fs/fsasyncawait.js
+++ fs/fsasyncawait.js
@@ -6,6 +6,12 @@ const filePath = path.join(__dirname, fileName);
 
 // Read Directory Structure
 
+/**
+ * Reads the current directory structure using fs.promises.readdir() and
+ * console.log's the result. If an error occurs, it is logged to the console.
+ *
+ * @returns {undefined}
+ */
 const readFilesWithAsyncAwait = async () => {
   try {
     const data = await fs.promises.readdir(__dirname);
