2261e4a05a25436b4933868a7e5c9af950462e6f
Saikat Bhadury <info2programmer@gmail.com>
2025-01-01 21:31:49 +0530

    Implement basic HTTP server with home and source code routes

---
+++ http_module/server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+// Create a webserver;
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Welcome to Saikat Bhadury Home Page</h1>");
+    res.end();
+  }
+  if (req.url === "/source-code") {
+    res.setHeader("Content-Type", "text/plain");
+    res.write("This is a source code page");
+    res.end();
+  }
+});
+
+// Listen Server
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Lisiening to PORT ${PORT}`);
+});
