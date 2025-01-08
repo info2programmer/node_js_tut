15073c7b23e4e75abfb9633903e05dc283b1fc8a
Saikat Bhadury <info2programmer@gmail.com>
2025-01-08 11:53:46 +0530

    Add documentation for random joke API request and enhance joke formatting

--- mini_project/random_joke.js
+++ mini_project/random_joke.js
@@ -1,6 +1,13 @@
 import http from "http";
 import chalk from "chalk";
 
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * Makes a GET request to the joke API and logs a random joke to the console.
+ * The joke is formatted with the setup in red and the punchline in blue
+ * with a red background.
+ */
+/******  a389c49a-b5a4-4f63-8e5e-0b4f6e82d3ae  *******/
 const getJoke = () => {
   const url = "http://www.official-joke-api.appspot.com/random_joke";
 
