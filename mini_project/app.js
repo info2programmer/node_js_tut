790219e9acde7a952c91659ce9128a01248132ba
Saikat Bhadury <info2programmer@gmail.com>
2025-01-08 11:11:30 +0530

    Implement file creation CLI with user input for filename and content

---
+++ mini_project/app.js
@@ -0,0 +1,31 @@
+import chalk from "chalk";
+import readline from "readline";
+import fs from "fs/promises";
+import path from "path";
+const __dirname = path.resolve();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const showFileNamePrompt = () => {
+  //   console.log(chalk.bgCyan("Enter file name"));
+  rl.question(chalk.bgCyan("Enter file name: "), handelPrompt);
+};
+
+const handelPrompt = (option) => {
+  const fileName = `${option}.txt`;
+  const filePath = path.join(__dirname, fileName);
+  rl.question(chalk.bgCyan("Enter file content: "), async (fileContent) => {
+    try {
+      await fs.writeFile(filePath, fileContent, "utf-8");
+      console.log(chalk.bgGreen(`\n${fileName} created successfully`));
+      rl.close();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+showFileNamePrompt();
