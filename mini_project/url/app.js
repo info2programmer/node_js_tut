fb98c668e438e55737a7883ab3e00c41ef6f477b
Saikat Bhadury <info2programmer@gmail.com>
2025-01-17 12:14:23 +0530

    Implement URL shortening feature with data storage and validation

--- mini_project/url/app.js
+++ mini_project/url/app.js
@@ -1,6 +1,7 @@
 import { createServer } from "http";
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import path from "path";
+import crypto from "crypto";
 
 const PORT = 3000;
 
@@ -16,6 +17,22 @@ const serverFile = async (res, filePath, contentType) => {
   }
 };
 
+const jsonFile = async () => {
+  try {
+    const filePath = path.resolve("data", "data.json");
+    const data = await readFile(filePath, "utf-8");
+    return JSON.parse(data || JSON.stringify({}));
+    // res.writeHead(200, { "Content-Type": "application/json" });
+    // res.end(data);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      writeFile(path.resolve("data", "data.json"), JSON.stringify({}));
+      return JSON.parse(JSON.stringify({}));
+    }
+    throw error;
+  }
+};
+
 const server = createServer(async (req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
@@ -26,6 +43,54 @@ const server = createServer(async (req, res) => {
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end("Not Found");
     }
+  } else if (req.method === "POST" && req.url === "/shorten") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", async () => {
+      // console.log(body);
+      const { url, custom_data } = JSON.parse(body);
+
+      // Field Validation Code Start
+      if (!url) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Missing url field");
+        return;
+      }
+      // Field Validation Code End
+
+      const loadData = await jsonFile();
+
+      const finalShortCode =
+        custom_data || crypto.randomBytes(4).toString("hex");
+      // console.log(Object.values(loadData).indexOf(url));
+      if (Object.hasOwn(loadData, finalShortCode)) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Key already exists");
+        return;
+      } else if (!Object.values(loadData).indexOf(url)) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("URL already exists");
+        return;
+      }
+
+      loadData[finalShortCode] = url;
+
+      await writeFile(
+        path.resolve("data", "data.json"),
+        JSON.stringify(loadData)
+      );
+
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Data added successfully");
+
+      // const filePath = path.resolve("public", "data.json");
+      // await readFile(filePath, "utf-8", (err, data) => {
+
+      // });
+    });
   } else {
     res.writeHead(405, { "Content-Type": "text/plain" });
     res.end("Method Not Allowed");
