c938268359437be10e12f0c64fa0af918f4410d9
Saikat Bhadury <info2programmer@gmail.com>
2025-01-24 21:25:24 +0530

    Add profile and article routes with dynamic greetings in Express application

--- express/index.js
+++ express/index.js
@@ -10,7 +10,26 @@ const staticPath = path.join(import.meta.dirname, "public");
 app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
-  return res.send("<h1>Hello World!</h1>");
+  const greetPageFile = path.join(import.meta.dirname, "public", "index.html");
+  return res.sendFile(greetPageFile);
+});
+
+app.get("/profile/:username", (req, res) => {
+  // const greetPageFile = path.join(import.meta.dirname, "public", "index.html");
+  // return res.sendFile(greetPageFile);
+  console.log(req.params);
+  return res.send(`<h1>Hello ${req.params.username}</h1>`);
+});
+
+app.get("/profile/:username/artical/:slug", (req, res) => {
+  // const greetPageFile = path.join(import.meta.dirname, "public", "index.html");
+  // return res.sendFile(greetPageFile);
+  console.log(req.params);
+  return res.send(
+    `<h1>Artical for ${req.params.slug.replace(/-/g, " ")} by ${
+      req.params.username
+    }</h1>`
+  );
 });
 
 app.get("/about", (req, res) => {
@@ -38,12 +57,7 @@ app.get("/contact", (req, res) => {
       </form>`);
 });
 
-app.get("/greet", (req, res) => {
-  const greetPageFile = path.join(import.meta.dirname, "public", "index.html");
-  return res.sendFile(greetPageFile);
-});
-
 // const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} : http://localhost:${PORT}`);
 });
