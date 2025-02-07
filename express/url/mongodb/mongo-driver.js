31679442f083b0f7ee4449e484193cbb975ee9aa
Saikat Bhadury <info2programmer@gmail.com>
2025-02-07 12:33:26 +0530

    Refactor MongoDB driver code by commenting out example CRUD operations for clarity

--- express/url/mongodb/mongo-driver.js
+++ express/url/mongodb/mongo-driver.js
@@ -16,15 +16,51 @@ await client.connect();
 const db = client.db("url_shortner_db");
 const url_connection = db.collection("urls");
 
-url_connection.insertMany([
-  {
-    name: "Neha",
-    age: 30,
-    city: "Bhubaneswar",
-  },
-  {
-    name: "Gopal Paul",
-    age: 35,
-    city: "Mangalgunj",
-  },
-]);
+//! Create
+// url_connection.insertMany([
+//   {
+//     name: "Saikat",
+//     age: 30,
+//     city: "Kolkata",
+//   },
+//   {
+//     name: "Neha",
+//     age: 30,
+//     city: "Bhubaneswar",
+//   },
+//   {
+//     name: "Gopal Paul",
+//     age: 35,
+//     city: "Mangalgunj",
+//   },
+// ]);
+
+//! Read
+// const userCursor = url_connection.find();
+
+// for await (const user of userCursor) {
+//   console.log(user);
+// }
+
+// const userCursor = await url_connection.find().toArray();
+// console.log(userCursor);
+//? Where Condition
+
+// const user = await url_connection.findOne({ age: 30 });
+//  console.log(user);
+// console.log(user.name);
+// console.log(user._id.toHexString());
+
+//! Update
+
+// await url_connection.updateOne(
+//   { name: "Neha" },
+//   { $set: { name: "Neha Ray Chaudhury" } }
+// );
+
+// await url_connection.updateMany({}, { $set: { role: "admin" } });
+
+// ! Delete
+// const result = await url_connection.deleteOne({ age: 35 });
+// console.log(`${result.deletedCount} Document deleted`);
+// await url_connection.deleteMany({ role: "admin" });
